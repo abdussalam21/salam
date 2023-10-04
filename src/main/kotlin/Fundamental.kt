452b0e61c52a7c222127b30a77c8b7362561afc9
Abdus Salam <abdus7630@gmail.com>
2023-10-04 16:46:46 +0700

    wafae

--- src/main/kotlin/Fundamental.kt
+++ src/main/kotlin/Fundamental.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val timeClose = 17
-    val timeNow = 7
+    val timeClose = 20
+    val timeNow = 60
     val isClosed = if (timeNow >= timeClose) "kelas sudah tutup" else "kelas sudah buka"
 
     println(isClosed)
